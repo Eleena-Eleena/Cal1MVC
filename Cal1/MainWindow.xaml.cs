80b727fc7e6fea6c35029e330f53a8224b783960
Smeyuha Eleena <cuculuya.nusha@outlook.com>
2023-03-02 23:24:43 +0300

    Добавьте файлы проекта.

---
+++ Cal1/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Cal1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public double a;
+        public double b;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Model.comboBox = ComboB;
+            ComboB.ItemsSource = Model.datalist;
+            
+        }
+
+        private void ComboB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Model.Combo = ComboB.SelectedIndex;
+            switch (ComboB.SelectedIndex)
+            {
+                case 0:
+                    TBSign.Text = "+";
+                    break;
+                case 1:
+                    TBSign.Text = "-";
+                    break;
+                case 2:
+                    TBSign.Text = "/";
+                    break;
+                case 3:
+                    TBSign.Text = "*";
+                    break;
+
+                default:
+                    break;
+
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+               a = double.Parse(TBa.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Неверное А");
+                return;
+            }
+
+            try
+            {
+                b = double.Parse(TBb.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Неверное В");
+                return;
+            }
+            switch (ComboB.SelectedIndex) 
+            {
+                case 0:
+                    TBoxRes.Text = Model.Add(a,b).ToString();
+                    break;
+                case 1:
+                    TBoxRes.Text = Model.Rem(a, b).ToString();
+                    break;
+                case 2:
+                    TBoxRes.Text = Model.Dev(a, b).ToString();
+                    break;
+                case 3:
+                    TBoxRes.Text = Model.Mul(a, b).ToString();
+                    break;
+
+                default:
+                    break;
+
+            }
+        }
+    }
+}
---
+++ Cal1/Model.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Cal1
+{
+    public static class Model
+    {
+        public static List<String> datalist = new List<string> { "Сложение", "Вычитание", "Деление", "Умножение" };
+        public static TextBlock tb;
+        public static ComboBox comboBox;
+
+        public static double Add(double x1, double x2)
+        {
+            return x1 + x2;
+        }
+        public static double Rem(double x1, double x2)
+        {
+            return x1 - x2;
+        }
+        public static double Mul(double x1, double x2)
+        {
+            return x1 * x2;
+        }
+        public static double Dev(double x1, double x2)
+        {
+            if (x2 !=0)
+            {
+                return x1 / x2;
+            }
+            else
+            {
+                MessageBox.Show("На 0 делить нельзя");
+                return 0;
+            }
+        }
+        public static int Combo
+        {
+            set
+            {
+                comboBox.Text = datalist[value];
+            }
+        }
+
+
+    }
+}
